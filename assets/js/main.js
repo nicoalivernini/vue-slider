ce0e5d7219cab505b179fa34c43aa64d37d575c0
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-24 18:08:28 +0100

    Esercizio completato + bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,28 +8,37 @@ var app = new Vue (
         'https://www.larepubblicadeglianimali.com/wordpress/wp-content/uploads/2019/08/incendi-amazzonia-conseguenze-animali-la-repubblica-degli-animali-805x527.jpg',
         'https://images2.corriereobjects.it/methode_image/2020/11/27/Scienze/Foto%20Scienze%20-%20Trattate/31.0.759478395-0005-k0QH-U322020628087643aH-656x492@Corriere-Web-Sezioni.jpg?v=20201127134756',
       ],
-      active: '',
     },
+
+    created() {
+      setInterval(() => {
+        this.next();
+      }, 2000);
+    },
+
     methods: {
+
       next: function () {
         if (this.index < this.imgSrc.length - 1) {
           this.index++;
         } else {
           this.index = 0;
-        }
+        };
       },
+
       prev: function (){
         if (this.index > 0) {
           this.index--;
         } else {
           this.index = this.imgSrc.length - 1;
-        }
+        };
       },
-      attiva: function (){
-        if (dot - 1 = this.index) {
-          this.active = 'active';
-        }
+
+      active: function (index){
+        this.index = index;
       },
+
+
     }
   }
-);
+); //Chiusura Vue
