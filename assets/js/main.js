7d41ac053b7bbabc6a146b0c1471b0175e29edb3
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-24 15:37:14 +0100

    Creo html e applico i click Vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,16 @@ var app = new Vue (
   {
     el:'#root',
     data: {
-      
+      imgSrc: [
+        'https://www.changethefuture.it/assets/uploads/2020/05/animali-selvatici.jpg',
+        'https://www.larepubblicadeglianimali.com/wordpress/wp-content/uploads/2019/08/incendi-amazzonia-conseguenze-animali-la-repubblica-degli-animali-805x527.jpg',
+        'https://images2.corriereobjects.it/methode_image/2020/11/27/Scienze/Foto%20Scienze%20-%20Trattate/31.0.759478395-0005-k0QH-U322020628087643aH-656x492@Corriere-Web-Sezioni.jpg?v=20201127134756',
+      ]
+    },
+    methods: {
+      prev: function () {
+        console.log('ok');
+      }
     }
   }
 );
