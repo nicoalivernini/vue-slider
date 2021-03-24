cdf419056806509b05fd437b053d7a349e2381d3
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-24 16:52:19 +0100

    Aggiornamento ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,8 @@ var app = new Vue (
         'https://www.changethefuture.it/assets/uploads/2020/05/animali-selvatici.jpg',
         'https://www.larepubblicadeglianimali.com/wordpress/wp-content/uploads/2019/08/incendi-amazzonia-conseguenze-animali-la-repubblica-degli-animali-805x527.jpg',
         'https://images2.corriereobjects.it/methode_image/2020/11/27/Scienze/Foto%20Scienze%20-%20Trattate/31.0.759478395-0005-k0QH-U322020628087643aH-656x492@Corriere-Web-Sezioni.jpg?v=20201127134756',
-      ]
+      ],
+      active: '',
     },
     methods: {
       next: function () {
@@ -17,11 +18,16 @@ var app = new Vue (
           this.index = 0;
         }
       },
-      prev: function () {
+      prev: function (){
         if (this.index > 0) {
           this.index--;
         } else {
           this.index = this.imgSrc.length - 1;
+        }
+      },
+      attiva: function (){
+        if (dot - 1 = this.index) {
+          this.active = 'active';
         }
       },
     }
